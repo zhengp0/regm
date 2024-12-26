78cf53002403742b5705cbf1d634e75cf2b2958f
zhengp0 <zhengp@uw.edu>
2024-12-26 14:40:49 -0800

    add __all__ to models.__init__

--- src/regmod/models/__init__.py
+++ src/regmod/models/__init__.py
@@ -10,3 +10,20 @@ from .pogit import PogitModel
 from .poisson import CanonicalPoissonModel, PoissonModel, create_poisson_model
 from .tobit import TobitModel
 from .weibull import WeibullModel
+
+__all__ = [
+    "BinomialModel",
+    "CanonicalBinomialModel",
+    "create_binomial_model",
+    "CanonicalGaussianModel",
+    "GaussianModel",
+    "create_gaussian_model",
+    "Model",
+    "NegativeBinomialModel",
+    "PogitModel",
+    "CanonicalPoissonModel",
+    "PoissonModel",
+    "create_poisson_model",
+    "TobitModel",
+    "WeibullModel",
+]
